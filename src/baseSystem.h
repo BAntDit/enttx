212560527a6fe27cfe44ea481616caba060778e5
BAntDit <bantdit@gmail.com>
2018-12-31 21:31:46 +0200

    shared library to interface library

--- src/baseSystem.h
+++ src/baseSystem.h
@@ -7,34 +7,37 @@
 
 #include <boost/cstdfloat.hpp>
 
-namespace enttx
+namespace enttx {
+template<class Derived>
+class BaseSystem
 {
-    template<class Derived>
-    class BaseSystem {
-    public:
-        template<typename... Args>
-        void init(Args&&... args);
-
-        void earlyUpdate(boost::float32_t dt);
-
-        void lateUpdate(boost::float32_t dt);
-    };
-
-    template<class Derived>
-    void BaseSystem<Derived>::earlyUpdate(boost::float32_t dt) {
-        static_cast<Derived*>(this)->earlyUpdate(dt);
-    }
-
-    template<class Derived>
-    void BaseSystem<Derived>::lateUpdate(boost::float32_t dt) {
-        static_cast<Derived*>(this)->lateUpdate(dt);
-    }
-
-    template<class Derived>
+public:
     template<typename... Args>
-    void BaseSystem<Derived>::init(Args&&... args) {
-        static_cast<Derived*>(this)->init(std::forward<Args>(args)...);
-    }
+    void init(Args&&... args);
+
+    void earlyUpdate(boost::float32_t dt);
+
+    void lateUpdate(boost::float32_t dt);
+};
+
+template<class Derived>
+void BaseSystem<Derived>::earlyUpdate(boost::float32_t dt)
+{
+    static_cast<Derived*>(this)->earlyUpdate(dt);
 }
 
-#endif //ENTTX_BASESYSTEM_H
+template<class Derived>
+void BaseSystem<Derived>::lateUpdate(boost::float32_t dt)
+{
+    static_cast<Derived*>(this)->lateUpdate(dt);
+}
+
+template<class Derived>
+template<typename... Args>
+void BaseSystem<Derived>::init(Args&&... args)
+{
+    static_cast<Derived*>(this)->init(std::forward<Args>(args)...);
+}
+}
+
+#endif // ENTTX_BASESYSTEM_H
