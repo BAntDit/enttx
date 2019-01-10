4ca8110a5fbaba25e24063f0ed289e0c842de5d1
BAntDit <bantdit@gmail.com>
2019-01-10 18:04:16 +0200

    default storage refactoring

--- src/componentStorage.h
+++ src/componentStorage.h
@@ -38,37 +38,100 @@ public:
 
     auto data() -> std::pair<Component*, size_t>;
 
+    auto capacity() const -> size_t { return store_.capacity(); }
+
+    auto size() const -> size_t { return store_.size(); }
+
 private:
-    std::vector<std::array<Component, CHUNK_SIZE>> store_;
+    std::vector<uint32_t> indices_;
+    std::vector<Component> store_;
+
+    uint32_t maxValidIndex_;
 };
 
 template<size_t CHUNK_SIZE, size_t INITIAL_CHUNK_COUNT, typename Component>
 ComponentStorage<CHUNK_SIZE, INITIAL_CHUNK_COUNT, Component>::ComponentStorage()
-  : store_{}
+  : indices_(CHUNK_SIZE * INITIAL_CHUNK_COUNT, std::numeric_limits<uint32_t>::max())
+  , store_{}
+  , maxValidIndex_{ std::numeric_limits<uint32_t>::max() }
 {
-    store_.resize(INITIAL_CHUNK_COUNT);
+    store_.reserve(CHUNK_SIZE * INITIAL_CHUNK_COUNT);
 }
 
 template<size_t CHUNK_SIZE, size_t INITIAL_CHUNK_COUNT, typename Component>
 auto ComponentStorage<CHUNK_SIZE, INITIAL_CHUNK_COUNT, Component>::get(uint32_t index) const -> Component const&
 {
-    assert(index < store_.size() * CHUNK_SIZE);
+    assert(index < indices_.size());
 
-    return *(store_[index / CHUNK_SIZE].data() + index % CHUNK_SIZE);
+    auto index_ = indices_[index];
+
+    assert(index_ < store_.size());
+
+    return store_[index_];
 }
 
 template<size_t CHUNK_SIZE, size_t INITIAL_CHUNK_COUNT, typename Component>
 auto ComponentStorage<CHUNK_SIZE, INITIAL_CHUNK_COUNT, Component>::get(uint32_t index) -> Component&
 {
-    assert(index < store_.size() * CHUNK_SIZE);
+    assert(index < indices_.size());
 
-    return *(store_[index / CHUNK_SIZE].data() + index % CHUNK_SIZE);
+    auto index_ = indices_[index];
+
+    assert(index_ < store_.size());
+
+    return store_[index_];
 }
 
 template<size_t CHUNK_SIZE, size_t INITIAL_CHUNK_COUNT, typename Component>
 template<typename... Args>
 auto ComponentStorage<CHUNK_SIZE, INITIAL_CHUNK_COUNT, Component>::create(uint32_t index, Args&&... args) -> Component&
 {
+    // TODO:: ...
+
+    {
+        auto size = indices_.size();
+
+        if (index >= size) {
+
+            size = index + 1;
+            size = size + CHUNK_SIZE - size % CHUNK_SIZE;
+
+            indices_.resize(size, std::numeric_limits<uint32_t>::max());
+        }
+    }
+
+    uint32_t pos = indices_[index];
+
+    if (pos == std::numeric_limits<uint32_t>::max()) {
+        for (auto it = indices_.begin(); it != indices_.end(); it++) {
+            if (*it == std::numeric_limits<uint32_t>::max()) continue;
+
+            auto distance = std::distance(indices_.begin(), it);
+
+            if (distance < index) {
+                if (pos == std::numeric_limits<uint32_t>::max()) {
+                    pos = *it + 1;
+                }
+            } else {
+                if (pos == std::numeric_limits<uint32_t>::max()) {
+                    pos = *it;
+                }
+
+                (*it)++;
+            }
+        }
+    }
+
+    indices_[index] = pos != std::numeric_limits<uint32_t>::max() ? pos : pos = 0;
+
+    {
+        auto capacity = store_.capacity();
+
+        if (pos >= store_.capacity()) {
+            // pos =
+        }
+    }
+
     return *(new (get(index)) Component(std::forward<Args>(args)...));
 }
 
