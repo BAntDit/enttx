10a1d1dd92b6482fb37a369d55ff8c900f74b54f
BAntDit <bantdit@gmail.com>
2019-01-24 23:59:14 +0200

    made possible to get to know whether system expect defined list of components or not

--- src/baseSystem.h
+++ src/baseSystem.h
@@ -6,17 +6,25 @@
 #define ENTTX_BASESYSTEM_H
 
 #include <cstddef>
+#include <easy-mp/type_list.h>
 
 namespace enttx {
 template<class Derived>
 class BaseSystem
 {
 public:
+    using tag_t = easy_mp::type_list<>;
+
     template<typename... Args>
     void init(Args&&... args);
 
     template<typename EntityManager, size_t STAGE>
     void update(EntityManager& entityManager);
+
+    template<typename... Components>
+    static constexpr bool is_in_use_v =
+      std::is_same_v<typename easy_mp::inner_join<typename Derived::tag_t, easy_mp::type_list<Components...>>::type,
+                     easy_mp::type_list<Components...>>;
 };
 
 template<class Derived>
