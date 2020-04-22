759b1cb35443551200d357a3abaa483ebaa3713a
BAntDit <bantdit@gmail.com>
2020-04-22 12:44:12 +0300

    shader refactoring

--- examples/particles/src/shaders/fragment.glsl
+++ examples/particles/src/shaders/fragment.glsl
@@ -37,44 +37,34 @@ float fbm(in vec3 p) {
     return n;
 }
 
-float sdShape(in vec3 p)
-{
-    float r = 0.6;
-    float amp = 0.4;
+float sdShape(in vec3 p, in float r) {
+    return length(p) - r;
+}
+
+float map(in vec3 p, in float r, in float amp) {
     float freq = 2.;
 
-    float d = length(p)
-        - mix(r * 0.5, mix(-0.5, r, clamp(time * 8, 0., 1.0)), 1. / max(1., (1.1 * time - .3)));
-
-    amp = mix(amp * .05, amp * 1.2, clamp(time * 4., 0., 1.));
-
-    d += fbm(p * freq + amp * time) * amp;
-
-    return d;
+    return sdShape(p, r) + fbm(p * freq + amp * time) * amp;
 }
 
-float map(in vec3 p) {
-    return sdShape(p);
-}
-
-vec4 raymarch(in vec3 ro, in vec3 stp) {
+vec4 explosion(in vec3 ro, in vec3 stp, in float exptime) {
     vec4 sum = vec4(0.0);
     vec3 pos = ro;
 
-    float density = 0.2;
-
-    density = mix(0., density, min(1., step(time, 0.25) + 1.0 / (2. * time)));
+    float density = mix(0., 0.2, min(1., step(exptime, 0.25) + 1.0 / (2. * exptime)));
+    float amp = mix(0.4 * .05, 0.4 * 1.2, clamp(exptime * 4., 0., 1.));
+    float r = mix(0.6 * 0.5, mix(-0.5, 0.6, clamp(exptime * 8, 0., 1.0)), 1. / max(1., (1.1 * exptime - .3)));
 
     for(int i = 0; i < 64; i++) {
-        float d = map(pos);
+        float d = map(pos, r, amp);
         vec4 col = vec4(0.);
 
         if (d < 0.2)
-            col = mix(vec4(vec3(2.), 1), vec4(1., 1., 0., 1.), d / 0.2);
+        col = mix(vec4(vec3(2.), 1), vec4(1., 1., 0., 1.), d / 0.2);
         else if (d >= 0.2 && d < 0.4)
-            col = mix(vec4(1, 1, 0, 1), vec4(.9, 0, 0, 0.5), (d - 0.2) / 0.2);
+        col = mix(vec4(1, 1, 0, 1), vec4(.9, 0, 0, 0.5), (d - 0.2) / 0.2);
         else if (d >= 0.4 && d < 0.6)
-            col = mix(vec4(1, 0, 0, 0.5), vec4(0, 0, 0, 0.4), (d - 0.4) / 0.2);
+        col = mix(vec4(1, 0, 0, 0.5), vec4(0, 0, 0, 0.4), (d - 0.4) / 0.2);
         // if (d >= 0.6 && d < 0.8)
         //     col = mix(vec4(0.2, 0.2, 0.2, 0.4), vec4(.5, .5, .5, 0.2), (d - 0.6) / 0.2);
         // else if (d >= 0.8 && d < 1.0)
@@ -90,6 +80,37 @@ vec4 raymarch(in vec3 ro, in vec3 stp) {
     }
 
     return sum;
+}
+
+vec4 bomb(in vec3 ro, in vec3 rd) {
+    float d = 0.1e-2;
+
+    for (int i = 0; i < 64 && d < 10.; i++) {
+        vec3 pos = ro + rd * d;
+
+        float h = map(pos, 0.6, 0.);
+
+        if (abs(h) < 0.1e-2) break;
+
+        d += h;
+    }
+
+    vec4 col = vec4(0.);
+
+    if (d < 10.) {
+        vec3 pos = ro + rd * d;
+        vec3 nor = normalize(pos);
+
+        col.rgb = nor;
+        col.a = 1.;
+    }
+
+    return col;
+}
+
+vec4 raymarch(in vec3 ro, in vec3 rd, in float stepSize) {
+    float delay = 4.;
+    return time < delay ? bomb(ro, rd) : explosion(ro, rd * stepSize, time - delay);
 }
 
 float weight(float z, float a) {
@@ -116,7 +137,7 @@ void main() {
 
     vec3 start = ro + rd * 5.; // make it closer to shape
 
-    col = raymarch(start, rd * delta);
+    col = raymarch(start, rd, delta);
 
     float w = weight(gl_FragCoord.z, col.a);
 
